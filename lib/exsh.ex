54518602729a31d5824ed9ac1cf515f6e53d9817
Christopher Stoll <stollcri@gmail.com>
2017-06-09 19:14:20 -0400

    Initial commit

---
+++ lib/exsh.ex
@@ -0,0 +1,18 @@
+defmodule Exsh do
+  @moduledoc """
+  Documentation for Exsh.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exsh.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
