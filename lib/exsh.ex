ad339c4d52b920b366692e12442c4bcd6c553ae8
Christopher Stoll <stollcri@gmail.com>
2017-06-25 14:54:38 -0600

    Break down methods to improve testability

--- lib/exsh.ex
+++ lib/exsh.ex
@@ -15,30 +15,11 @@ defmodule Exsh do
   end
 
   @doc """
-  Parse the passed in arguments to build options and symbol maps
+  Parse the command line `arguments` to build an option map, a symbol map, and a command list
 
   Returns `{options, symbols, commands}`
-
-  ## Examples
-
-    iex> Exsh.parse_args(["-s", "a=a", "--symbol", "b=b", "--exit", "symbols"])
-    { \
-      %{ \
-        exit: true, \
-        help: false, \
-        noise: 0, \
-        nosymbols: false, \
-        prompt: "\e[32m> \e[0m", \
-        version: "mk II, rev 1, no 1"}, \
-      %{ \
-        "a" => "a", \
-        "b" => "b" \
-      }, \
-      ["symbols"] \
-    }
-
   """
-  def parse_args(args) do
+  def parse_args(arguments) do
     options_default = %{
       :version => Application.get_env(:exsh, :version),
       :prompt => IO.ANSI.green <> "> " <> IO.ANSI.reset,
@@ -49,7 +30,7 @@ defmodule Exsh do
       :quiet => 0,
       :exit => :false
     }
-    {options_input, commands, _} = OptionParser.parse(args,
+    {options_input, commands, _} = OptionParser.parse(arguments,
       strict: [
         help: :boolean,
         loud: :count,
@@ -68,8 +49,34 @@ defmodule Exsh do
     )
 
     # set symbols and options based upon command line arguments
-    options = Enum.into(options_input, options_default)
+    options = options_from_options(options_input, options_default)
     symbols = symbols_from_options(options_input)
+
+    if options[:help] do
+      {Enum.into(options, [:exit, :true]), symbols, ["help"]}
+    else
+      {options, symbols, commands}
+    end
+  end
+
+  @doc """
+  Get application options from command line `options` or `defaults`
+
+  Returns `{options}`
+
+  ## Examples
+
+    iex> Exsh.options_from_options([], %{loud: 0, quiet: 0})
+    %{:noise => 0}
+    iex> Exsh.options_from_options([exit: true], %{exit: false, loud: 0, quiet: 0})
+    %{:exit => true, :noise => 0}
+    iex> Exsh.options_from_options([symbol: "a=a", exit: true], %{exit: false, loud: 0, quiet: 0})
+    %{:exit => true, :noise => 0}
+
+  """
+  def options_from_options(options, defaults) do
+    # set options given command line arguments and defaults
+    options = Enum.into(options, defaults)
 
     # let's not deal with both loud and quiet everywhere
     # the noise option should be used to get verbosity
@@ -79,14 +86,31 @@ defmodule Exsh do
     # drop options which are no longer needed
     options = Map.drop(options, [:loud, :symbol, :quiet])
 
-    if options[:help] do
-      {Enum.into(options, [:exit, :true]), symbols, ["help"]}
-    else
-      {options, symbols, commands}
-    end
+    options
   end
 
-  defp symbols_from_options(options) do
+  @doc """
+  Get application symbols from command line `options`
+
+  Returns `{symbols}`
+
+  ## Examples
+
+    iex> Exsh.symbols_from_options([symbol: "a=a"])
+    %{"a" => "a"}
+    iex> Exsh.symbols_from_options([symbol: "a=a", symbol: "b=b"])
+    %{"a" => "a", "b" => "b"}
+    iex> Exsh.symbols_from_options([quiet: 1, symbol: "a=a", symbol: "b=b", exit: true])
+    %{"a" => "a", "b" => "b"}
+    iex> Exsh.symbols_from_options([quiet: 1])
+    %{}
+    iex> Exsh.symbols_from_options([symbol: "a"])
+    %{}
+    iex> Exsh.symbols_from_options([symbol: "a", symbol: "b=b"])
+    %{"b" => "b"}
+
+  """
+  def symbols_from_options(options) do
     symbols_from_options(options, %{})
   end
   defp symbols_from_options([], symbols) do
@@ -98,17 +122,33 @@ defmodule Exsh do
     new_symbols = Map.merge(symbols, new_symbol)
     symbols_from_options(remaining_options, new_symbols)
   end
+
   defp symbol_from_option(option) do
-    if elem(option, 0) == :symbol do
-      [var | tail] = String.split(elem(option, 1), "=")
-      [val | _] = tail
-      %{var => val}
-    else
-      %{}
-    end
+    symbol_from_option(elem(option, 0), elem(option, 1))
+  end
+  defp symbol_from_option(:symbol, symbol_expression) do
+    [var | val_list] = String.split(symbol_expression, "=")
+    symbol_from_symbol_expression(var, val_list)
+  end
+  defp symbol_from_option(_, _) do
+    %{}
   end
 
-  defp parse_settings({options, symbols, commands}) do
+  defp symbol_from_symbol_expression(_, []) do
+    %{}
+  end
+  defp symbol_from_symbol_expression(var, val_list) do
+    [val | _] = val_list
+    %{var => val}
+  end
+
+  @doc """
+  Adjust options and symbols based upon other settings
+  (presently only adds hard coded values)
+
+  Returns `{options, symbols, commands}`
+  """
+  def parse_settings({options, symbols, commands}) do
     hard_coded_symbols = %{
       "alias" => "symbols",
       "env" => "symbols",
